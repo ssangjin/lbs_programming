c01e6a16c6c3f755bc633c8ceb1333ccf99dda73
Sangjin <sangjin.yun@gmail.com>
2018-05-28 23:35:05 +0900

    2-1 sensor 수강생용 완성

--- LBS_2_1/app/src/main/java/com/lbs/programming/lbs_2_1/MainActivity.java
+++ LBS_2_1/app/src/main/java/com/lbs/programming/lbs_2_1/MainActivity.java
@@ -31,45 +31,15 @@ public class MainActivity extends AppCompatActivity {
     private long lastAccelerationEventTime = 0;
     private long lastRotationEventTime = 0;
 
-    private SensorEventListener sensorEventListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            switch (event.sensor.getType()) {
-                case Sensor.TYPE_ACCELEROMETER:
-                    if (System.currentTimeMillis() - lastAccelerationEventTime > 100) {
-                        lastAccelerationEventTime = System.currentTimeMillis();
-                        textViewValueX.setText(String.format("%3.2f", event.values[0]));
-                        textViewValueY.setText(String.format("%3.2f", event.values[1]));
-                        textViewValueZ.setText(String.format("%3.2f", event.values[2]));
-                    }
-                    break;
-                case Sensor.TYPE_ROTATION_VECTOR:
-                    if (System.currentTimeMillis() - lastRotationEventTime > 100) {
-                        lastRotationEventTime = System.currentTimeMillis();
-
-                        textViewRotationValueX.setText(String.format("%3.2f", event.values[0]));
-                        textViewRotationValueY.setText(String.format("%3.2f", event.values[1]));
-                        textViewRotationValueZ.setText(String.format("%3.2f", event.values[2]));
-                    }
-                    break;
-                case Sensor.TYPE_STEP_COUNTER:
-                    textViewStepCountValue.setText(String.format("%8.0f", event.values[0]));
-                    break;
-            }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
+    // TODO: Sensor Event Listener 구현.
+    private SensorEventListener sensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        // TODO: SensorManager 가져오기.
 
         showSensorList();
 
@@ -85,29 +55,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showSensorList() {
-        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        for (Sensor sensor : sensorList) {
-            Log.e("SensorList", sensor.toString());
-        }
+        // TODO: Sensor List 출력
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        accelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(sensorEventListener, accelerationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        // TODO: Accelerometer 등록
 
-        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        sensorManager.registerListener(sensorEventListener, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        // TODO: Rotation vector 등록
 
-        stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        sensorManager.registerListener(sensorEventListener, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        // TODO: step counter 등록
     }
 
     @Override
     protected void onPause() {
-        sensorManager.unregisterListener(sensorEventListener);
+        // TODO: unregister sensor listener
         super.onPause();
     }
 }
