912207efc2aa2c617648821b21872f8fdf606891
Sangjin <sangjin.yun@gmail.com>
2018-05-29 20:39:47 +0900

    2-2 WIFI SCAN 완료.

---
+++ LBS_2_2/app/src/main/java/com/lbs/programming/lbs_2_2/MainActivity.java
@@ -0,0 +1,92 @@
+package com.lbs.programming.lbs_2_2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private WifiManager wifiManager;
+    private WifiScanReceiver wifiScanReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        wifiScanReceiver = new WifiScanReceiver();
+        registerReceiver(wifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        onScan(null);
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(wifiScanReceiver);
+        super.onPause();
+    }
+
+    public void onScan(View view) {
+        wifiManager.startScan();
+    }
+
+    private class WifiScanReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<ScanResult> scanResultList = wifiManager.getScanResults();
+            TableLayout table = (TableLayout) findViewById(R.id.tableLayout);
+            table.removeAllViews();
+
+            TableRow row = new TableRow(getBaseContext());
+            Field[] fields = ScanResult.class.getDeclaredFields();
+            for (Field field : fields) {
+                TextView textView = new TextView(getBaseContext());
+                textView.setText("  "+ field.getName() + "  ");
+                textView.setBackgroundColor(Color.GRAY);
+                row.addView(textView);
+            }
+            table.addView(row);
+
+            for(ScanResult scanResult : scanResultList) {
+                // create a new TableRow
+                row = new TableRow(getBaseContext());
+
+                for (Field field : fields) {
+                    TextView textView = new TextView(getBaseContext());
+                    try {
+                        Object value = field.get(scanResult);
+
+                        if (value == null) {
+                            textView.setText("");
+                        } else {
+                            textView.setText(value.toString());
+                        }
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    row.addView(textView);
+                }
+
+                table.addView(row);
+            }
+        }
+    }
+}
