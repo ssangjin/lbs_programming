f871241c0b7687a3e68716696bb265d2fce434e5
Sangjin Yun <sangjin.yun@sk.com>
2019-09-17 06:55:58 +0900

    2-1 코드 제거

--- LBS_2_1/app1/src/main/java/com/lbs/programming/lbs_2_1/MainActivity.java
+++ LBS_2_1/app1/src/main/java/com/lbs/programming/lbs_2_1/MainActivity.java
@@ -41,88 +41,27 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.buttonHash:
                 // TODO: show hash code.
-                int hash = input.getText().hashCode();
-                output.setText(Integer.toString(hash));
                 break;
         }
     }
 
     private String encode(String originalText, String key) {
-
-        try {
-            Cipher cipher = getCipher();
-            byte[] keyBytes = getKeyBytes(key.getBytes());
-
-            SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
-
-            try {
-                cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-            } catch (InvalidKeyException e) {
-                e.printStackTrace();
-            }
-
-
-            byte[] encrypted = cipher.doFinal(originalText.getBytes());
-
-            return Base64.encodeToString(encrypted, Base64.DEFAULT);
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        // TODO: encode text
         return null;
     }
 
     private Cipher getCipher() throws NoSuchAlgorithmException, NoSuchPaddingException {
-        Cipher cipher;
-        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        return cipher;
+        // TODO: Cipher 가져오기
+        return null;
     }
 
     private byte[] getKeyBytes(byte[] bytes) {
-        byte[] keyBytes = new byte[16];
-        byte[] bkey = bytes;
-
-        int length = bkey.length;
-        if (length > keyBytes.length) {
-            length = keyBytes.length;
-        }
-
-        System.arraycopy(bkey, 0, keyBytes, 0, length);
-        return keyBytes;
+        // TODO: KEY의 byte array 가져오기
+        return null;
     }
 
     private String decode(String encryptedText, String key) {
-        try {
-            byte[] keyBytes = getKeyBytes(key.getBytes("UTF-8"));
-            SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, "AES");
-            Cipher cipher = getCipher();
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-
-            byte[] original = cipher.doFinal(Base64.decode(encryptedText, Base64.DEFAULT));
-            return new String(original);
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        // TODO: decode text.
         return null;
     }
 }
