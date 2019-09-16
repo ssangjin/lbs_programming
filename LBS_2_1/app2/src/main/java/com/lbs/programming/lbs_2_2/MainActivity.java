1ac34bb21c9f92c1c955388c978228f1e7e76667
Sangjin Yun <sangjin.yun@sk.com>
2019-09-17 07:01:54 +0900

    App 2-2 코드 제거

--- LBS_2_1/app2/src/main/java/com/lbs/programming/lbs_2_2/MainActivity.java
+++ LBS_2_1/app2/src/main/java/com/lbs/programming/lbs_2_2/MainActivity.java
@@ -38,22 +38,18 @@ public class MainActivity extends AppCompatActivity {
                 case Sensor.TYPE_ACCELEROMETER:
                     if (System.currentTimeMillis() - lastAccelerationEventTime > 100) {
                         lastAccelerationEventTime = System.currentTimeMillis();
-                        textViewValueX.setText(String.format("%3.2f", event.values[0]));
-                        textViewValueY.setText(String.format("%3.2f", event.values[1]));
-                        textViewValueZ.setText(String.format("%3.2f", event.values[2]));
+                        // TODO: Acceleration Sensor 값 표출
                     }
                     break;
                 case Sensor.TYPE_ROTATION_VECTOR:
                     if (System.currentTimeMillis() - lastRotationEventTime > 100) {
                         lastRotationEventTime = System.currentTimeMillis();
 
-                        textViewRotationValueX.setText(String.format("%3.2f", event.values[0]));
-                        textViewRotationValueY.setText(String.format("%3.2f", event.values[1]));
-                        textViewRotationValueZ.setText(String.format("%3.2f", event.values[2]));
+                        // TODO: Rotation vector 표출
                     }
                     break;
                 case Sensor.TYPE_STEP_COUNTER:
-                    textViewStepCountValue.setText(String.format("%8.0f", event.values[0]));
+                    // TODO: Step Counter 표출
                     break;
             }
         }
@@ -69,8 +65,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager)
-                getSystemService(Context.SENSOR_SERVICE);
+        // TODO: SensorManager 가져오기
 
         showSensorList();
 
@@ -86,30 +81,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showSensorList() {
-        List<Sensor> sensorList =
-                sensorManager.getSensorList(Sensor.TYPE_ALL);
-        for (Sensor sensor : sensorList) {
-            Log.e("SensorList", sensor.toString());
-        }
+        // TODO: Sensor list 확인
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        accelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(sensorEventListener, accelerationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        // TODO: Acceleration 등록
 
-        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        sensorManager.registerListener(sensorEventListener, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        // TODO: Rotation vector 등록
 
-        stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        sensorManager.registerListener(sensorEventListener, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        // TODO: Step Counter 등록
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(sensorEventListener);
+        // TODO: Sensor 해제
     }
 }
