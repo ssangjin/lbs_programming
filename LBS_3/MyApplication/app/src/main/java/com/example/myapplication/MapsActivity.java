3bd5171cf39f11b5b2c297a66451b6437fbc02a1
Sangjin Yun <sangjin.yun@sk.com>
2019-10-25 20:57:01 +0900

    Volley를 이용하여 데이터 저장 호출

--- LBS_3/MyApplication/app/src/main/java/com/example/myapplication/MapsActivity.java
+++ LBS_3/MyApplication/app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -1,15 +1,26 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Looper;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.*;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,10 +31,23 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 0;
+    private static String deviceId = "";
 
     private GoogleMap mMap;
+    RequestQueue queue;
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            onLocationChanged(locationResult.getLastLocation());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +58,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_LOCATION);
-        } else {
-            startLocationService();
-        }
+        queue = Volley.newRequestQueue(this);
     }
 
     @Override
@@ -75,12 +92,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         getLastLocation();
 
         // new Google API SDK v11 uses getFusedLocationProviderClient(this)
-        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                onLocationChanged(locationResult.getLastLocation());
-            }
-        }, Looper.myLooper());
+        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+        } else {
+            startLocationService();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        LocationServices.getFusedLocationProviderClient(this).removeLocationUpdates(locationCallback);
     }
 
     private void getLastLocation() {
@@ -105,7 +138,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
             // 로그를 저장한다.
+
+            sendLog(location);
         }
+    }
+
+    public static String getDeviceId(Context context) {
+        if (TextUtils.isEmpty(deviceId)) {
+            deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
+
+        return deviceId;
+    }
+
+    private void sendLog(Location location) {
+        String url = null;
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("http://192.168.1.187:8080/collect_data?");
+        sb.append("userId=");
+        sb.append(getDeviceId(this));
+        sb.append("&latitude=");
+        sb.append(location.getLatitude());
+        sb.append("&longitude=");
+        sb.append(location.getLongitude());
+        sb.append("&provider=");
+        sb.append(location.getProvider());
+        sb.append("&accuracy=");
+        sb.append(location.getAccuracy());
+
+        Log.d("Result", "url:" + sb.toString());
+
+        url = sb.toString();
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("Result", "response:" + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Result", "on Error:" + error.toString());
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 
     /**
@@ -126,5 +209,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setMyLocationEnabled(true);
+    }
+
+    public void loadData(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
     }
 }
