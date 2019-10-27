39f664d0ef92555d7baf1ffa5d0c1b30288ea01c
Sangjin Yun <sangjin.yun@sk.com>
2019-10-27 15:22:56 +0900

    서버의 상점 데이터 가져오기 수행.

--- LBS_3/location/src/main/java/com/example/RequestStoreData.java
+++ LBS_3/location/src/main/java/com/example/RequestStoreData.java
@@ -4,9 +4,9 @@ import com.example.daos.DatastoreDao;
 import com.example.daos.LocationDataDao;
 import com.example.data.LocationData;
 import com.example.data.Result;
+import com.google.appengine.repackaged.com.google.common.base.Stopwatch;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import javax.servlet.ServletException;
@@ -16,32 +16,103 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
-@WebServlet(name = "RequestData", value = "/request_data")
-public class RequestData extends HttpServlet {
+@WebServlet(name = "RequestStoreData", value = "/request_store_data")
+public class RequestStoreData extends HttpServlet {
+    Connection conn;
+
+    @Override
+    public void init() throws ServletException {
+        String url = System.getProperty("cloudsql");
+
+        // Server Deploy 설정
+//        log("connecting to: " + url);
+//        try {
+//            conn = DriverManager.getConnection(url);
+//        } catch (SQLException e) {
+//            throw new ServletException("Unable to connect to Cloud SQL", e);
+//        }
+
+        // Local 설정
+        url = "jdbc:mysql://localhost:3306/lbs?user=root";
+        log("connecting to: " + url);
+        try {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("com.mysql.jdbc.GoogleDriver");
+                Class.forName("com.google.cloud.sql.jdbc.Driver");
+
+            } catch (ClassNotFoundException e) {
+            }
+
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            throw new ServletException("Unable to connect to Cloud SQL", e);
+        }
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("euc-kr");
         PrintWriter out = resp.getWriter();
         Gson gson = new Gson();
 
+        String code = req.getParameter("code");
+        Double lat = Double.parseDouble(req.getParameter("lat"));
+        Double lon = Double.parseDouble(req.getParameter("lon"));
+
+        String sql = "select * from StoreInfromation \n" +
+                "where commercial_type_classification_code1 = ? \n" +
+                "and longitude > ? and longitude < ? \n" +
+                "and latitude > ? and latitude < ? limit 500\n";
+        PreparedStatement pstmt = null;
         try {
-            String userId = req.getParameter(LocationData.USER_ID);
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, code);
+            pstmt.setDouble(2, lon - 0.1);
+            pstmt.setDouble(3, lon + 0.1);
+            pstmt.setDouble(4, lat - 0.1);
+            pstmt.setDouble(5, lat + 0.1);
 
-            LocationDataDao dao = dao = new DatastoreDao();
-            try {
+            Stopwatch stopwatch = Stopwatch.createStarted();
+            try (ResultSet rs = pstmt.executeQuery()) {
+                stopwatch.stop();
+
                 JsonArray jsonArray = new JsonArray();
-                Result<LocationData> locationDataResult = dao.listLocationDatas(userId);
-                for (LocationData location : locationDataResult.result) {
-                    jsonArray.add(gson.toJsonTree(location));
-                }
-                out.println(jsonArray.toString());
-            } catch (Exception e) {
-                throw new ServletException("Error to get LocationData", e);
-            }
 
-        } catch (Exception e) {
-            out.println(e.getMessage());
+                while (rs.next()) {
+                    StoreInfo storeInfo = new StoreInfo();
+
+                    storeInfo.name = rs.getString("trade_name");
+                    storeInfo.className = rs.getString("\"commercial type classification code2\"");
+                    storeInfo.businessName = rs.getString("\"Commercial business classification name\"");
+                    storeInfo.industrialName = rs.getString("\"standard industrial classification name\"");
+                    storeInfo.latitude = rs.getDouble("latitude");
+                    storeInfo.longitude = rs.getDouble("longitude");
+
+                    jsonArray.add(gson.toJsonTree(storeInfo));
+                }
+
+                out.println(jsonArray.toString());
+
+            } catch (SQLException ex) {
+                throw new ServletException("SQL error", ex);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
+
+    static class StoreInfo {
+        String name;
+        String className;
+        String businessName;
+        String industrialName;
+        Double latitude;
+        Double longitude;
     }
 }
